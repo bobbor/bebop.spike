f4265f927669f468f547b69a8ef7e28c727e88d2
Philipp Paul <me@philipp-paul.com>
2015-07-13 11:21:50 +0200

    umd, README and formatting

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# dom.view
-backbone-View that uses dom as framework
+## bebop.spike
+
+### bebop?
+
+[bebop][1] is a small framework for DOM-Manipulation and AJAX.  
+It also has support for Promises.
+
+### so?
+
+Backbone uses jQuery per default as tool for DOM-Traversal, Event-Binding and AJAX.  
+But I want to use bebop.
+
+---
+
+In the Backbone wiki exists a page which describes what is required to replace jQuery.  
+[Using Backbone without jQuery][2]
+
+So this is basically an implementation for [bebop][1]
+
+[1]: http://www.github.com/bobbor/bebop
+[2]: https://github.com/jashkenas/backbone/wiki/Using-Backbone-without-jQuery
--- view.js
+++ view.js
@@ -1,40 +1,47 @@
-// we have to overwrite [certain functions](https://github.com/jashkenas/backbone/wiki/Using-Backbone-without-jQuery)
-// to allow backbone to work without jquery
-(function(root, factory) {
-    'use strict';
+// Uses AMD or browser globals to create a module. This example creates a
+// global even when AMD is used. This is useful if you have some scripts
+// that are loaded by an AMD loader, but they still want access to globals.
+// for ref: https://github.com/umdjs/umd/blob/master/amdWebGlobal.js
+
+(function (root, factory) {
     if (typeof define === 'function' && define.amd) {
-        define(['backbone', 'underscore', 'dom'], function () {
-            factory.apply(root, arguments);
+        // AMD. Register as an anonymous module.
+        define(['backbone', 'underscore', 'bebop'], function (Backbone, _, bebop) {
+            // Also create a global in case some scripts
+            // that are loaded still are looking for
+            // a global even when an AMD loader is in use.
+            return (Backbone.View = factory(Backbone, _, bebop));
         });
     } else {
-        factory(root.Backbone, root._, root.DOM);
+        // Browser globals
+        Backbone.View = factory(root.Backbone, root._, root.bebop);
     }
-}(this, function(Backbone, _, $) {
+}(self, function (Backbone, _, $) {
     'use strict';
     var hasOwn = {}.hasOwnProperty;
     var eventMap = {};
 
     var View = {
         // the "$" function
-        $                : function(sel) {
+        $: function (sel) {
             return this.$el.all(sel);
         },
         // removing an element is extremely simple
-        _removeElement   : function() {
+        _removeElement: function () {
             this.$el.remove();
         },
         // delegate = simple event binding
-        delegate         : function(eventName, selector, listener) {
+        delegate: function (eventName, selector, listener) {
             selector = selector || '';
             var k = eventName + '.delegateEvents' + this.cid + ':' + selector;
-            var proxylistener = function() {
+            var proxylistener = function () {
                 return listener.apply(this, arguments);
             };
             eventMap[k] = proxylistener;
             this.$el.on(eventName, proxylistener, selector);
         },
         // undelegate = removing single events
-        undelegate       : function(eventName, selector) { // don't need listener here, as identification is done via selector
+        undelegate: function (eventName, selector) { // don't need listener here, as identification is done via selector
             selector = selector || '';
             var k = eventName + '.delegateEvents' + this.cid + ':' + selector;
             var fn = eventMap[k];
@@ -44,15 +51,15 @@
             }
         },
         // undelegateEvents = removing all events
-        undelegateEvents : function() {
+        undelegateEvents: function () {
             var that = this;
             if (this.el) {
                 _.chain(eventMap)
                     .keys()
-                    .filter(function(key) {
+                    .filter(function (key) {
                         return key.indexOf('.delegateEvents' + that.cid) !== -1;
                     })
-                    .forEach(function(event) {
+                    .forEach(function (event) {
                         var name = event.replace(/\.delegateEvents(.*)$/g, '');
                         var fn = eventMap[event];
                         that.$el.off(name, fn);
@@ -62,12 +69,12 @@
             return this;
         },
         // setting the Element `this.el` of the view
-        _setElement      : function(el) {
+        _setElement: function (el) {
             this.$el = $.one(el);
             this.el = this.$el._node;
         },
         // setting Attributes - passed in the view
-        _setAttributes   : function(attributes) {
+        _setAttributes: function (attributes) {
             for (var attr in attributes) {
                 if (hasOwn.call(attributes, attr)) {
                     this.$el.attr(attr, attributes[attr]);
@@ -75,5 +82,5 @@
             }
         }
     };
-    Backbone.View = Backbone.View.extend(View);
+    return Backbone.View.extend(View);
 }));
